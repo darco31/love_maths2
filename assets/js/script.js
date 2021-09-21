07e7656f42e0be0767f235546c833d20420bd2eb
Stephen <sdarcy29@hotmail.com>
2021-09-21 16:12:51 +0000

    Add function function to rungame and setup when the page loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked submit')
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                rungame(gameType)
             }
         })
     }
+
+    rungame(gameType);
+
 })
 
 
 
-function rungame() {
+function rungame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1, num2);
+    } else {
+        alert(`Unknown game Type: ${gameType}`);
+        throw `Unknown game type: ${gameType}.Aborting!`
+    }
 }
 
 
@@ -37,18 +49,27 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
 }
 
-function displayMultiplyQuestion() {
+// function displayMultiplyQuestion() {
+//     document.getElementById('operand1').textContent = operand1;
+//     document.getElementById('operand2').textContent = operand2;
+//     document.getElementById('operator').textContent = '*';
+// }
 
-}
-
-function displayDivisionQuestion() {
-
-}
+// function displayDivisionQuestion() {
+//     document.getElementById('operand1').textContent = operand1;
+//     document.getElementById('operand2').textContent = operand2;
+//     document.getElementById('operator').textContent = '/';
+// }
