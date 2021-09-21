4a2bf56147a0c024f85ed4c710e9ae6d2a796c50
Stephen <sdarcy29@hotmail.com>
2021-09-21 15:34:37 +0000

    Add function function to rungame and setup when the page loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-function rungame() {
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
 
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('You clicked submit')
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
+
+function rungame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1
 }
+
 
 function checkAnswer() {
 
